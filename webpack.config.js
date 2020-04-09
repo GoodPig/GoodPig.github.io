c493650fccb664df6d0a9b5f2564f055a8094d39
0096001930 <0096001930@zte.com.cn>
2020-04-09 14:45:55 +0800

    tijiao

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'web'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.jsx$/, use: 'babel-loader' }
+        ]
+    }
+};
